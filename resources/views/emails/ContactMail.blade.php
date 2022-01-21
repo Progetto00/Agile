624081684e8762c766575bf656b1f6ec87e05bf6
mauriziodicarlo1999 <maurizio.dicarlo1999@gmail.com>
2022-01-21 09:40:54 +0100

    ContactMail.blade(messaggio vero e proprio)

---
+++ resources/views/emails/ContactMail.blade.php
@@ -0,0 +1,9 @@
+<p>Salve sono {{ $details['name'] }}</p>
+<br/>Dati utente:
+-Telefono{{ $details['phone'] }}
+-Email {{ $details['email'] }}
+<br/>
+
+<p>Sto contattando la Globex Corp per: {{ $details['msg'] }}</p>
+<br/>
+<p>Cordiali saluti,{{ $details['name'] }}.</p>
